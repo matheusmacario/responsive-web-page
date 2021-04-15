2a219d7af68a6c49da45c62a2abb0a48e3edeb2f
Matheus Macario <macariofilho96@gmail.com>
2021-04-15 11:17:26 -0300

    initial commit
    html base , style and burger, without JS animation

---
+++ ramadam.js
@@ -0,0 +1,9 @@
+
+//burger click
+const menutoggle = document.querySelector('.toggle')
+const navigation = document.querySelector('.navigation')
+menutoggle.onclick = function(){
+    menutoggle.classList.toggle('active')
+    navigation.classList.toggle('active')
+}
+
